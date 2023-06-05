331e3d2fa72183f046cab4d66b620f94e6009f6c
emandats <125905415+emandats@users.noreply.github.com>
2023-06-05 17:50:41 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+<div class="resume-button">
+            <button type="button"
+                onclick="window.location.href ='https://docs.google.com/document/d/1H58zt9kfoJMMixvvctPqq1aHgyLdC1BSecXRKEVdr1I/edit?usp=share_link';">Resume</button>
+        </div>
